87c49b40bf6f02b950ee4494d6a4050b87460766
ZH Motions <zhmotions@gmail.com>
2026-06-16 00:52:06 +0600

    v6.3.9: Free+Pro license

--- zh_downloader.py
+++ zh_downloader.py
@@ -48,7 +48,7 @@ except ImportError:
 
 # -- Constants --------------------------------------------------------------
 APP_NAME    = "ZH Downloader"
-APP_VER     = "6.3.8"
+APP_VER     = "6.3.9"
 APP_AUTHOR  = "ZH Motions"
 APP_URL     = "https://zhmotions.com"
 BRIDGE_PORT = 9613
